acea0d2d4a02feff3cca53a47b1315f16be8b80d
Nada Assem <105822551+Nada-Assem@users.noreply.github.com>
2024-01-26 00:11:14 +0200

    Add files via upload

---
+++ operator overloading/operator overloading/Complex.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace operator_overloading
+{
+    public class Complex
+    {
+        public int Real { get; set; }
+        public int Image { get; set; }
+
+        public override string ToString()
+        {
+            return $"{Real} + {Image}i";
+        }
+        public static Complex operator +(Complex Left, Complex Right)
+        {
+            return new Complex
+            {
+                Real = (Left?.Real ?? 0) + (Right?.Real??0),
+                Image = (Left?.Image ?? 0) + (Right?.Image ?? 0)
+            };
+        }
+        public static Complex operator -(Complex Left, Complex Right)
+        {
+            return new Complex
+            {
+                Real = (Left?.Real ?? 0) - (Right?.Real ?? 0),
+                Image = (Left?.Image??0) - (Right?.Image ?? 0)
+            };
+        }
+
+        public static Complex operator +(Complex Left, int Right)
+        {
+            return new Complex
+            {
+                Real = (Left?.Real ?? 0) + Right ,
+                Image = (Left?.Image ?? 0)
+            };
+        }
+
+        public static Complex operator +(int Left, Complex Right)
+        {
+            return new Complex
+            {
+                Real = (Right?.Real ?? 0) + Left,
+                Image = (Right?.Image ?? 0)
+            };
+        }
+
+        public static Complex operator -(Complex Left, int Right)
+        {
+            return new Complex
+            {
+                Real = (Left?.Real ?? 0) - Right,
+                Image = (Left?.Image ?? 0)
+            };
+        }
+
+        public static Complex operator -(int Left, Complex Right)
+        {
+            return new Complex
+            {
+                Real = (Right?.Real ?? 0) - Left,
+                Image = (Right?.Image ?? 0)
+            };
+        }
+        public static Complex operator ++(Complex complex)
+        {
+            return new Complex
+            {
+                Real = (complex?.Real ?? 0) + 1,
+                Image = (complex?.Image ?? 0)
+            };
+        }
+        public static Complex operator --(Complex complex)
+        {
+            return new Complex
+            {
+                Real = (complex?.Real ?? 0) - 1,
+                Image = (complex?.Image ?? 0)
+            };
+        }
+        public static bool operator ==(Complex left , Complex Right)
+        {
+            if (left.Real == Right.Real && left.Image == Right.Image)
+                return true;
+            return false;
+
+        }
+        public static bool operator !=(Complex left, Complex Right)
+        {
+            if (left.Real != Right.Real || left.Image != Right.Image)
+                return true;
+            return false;
+
+        }
+    }
+}
---
+++ operator overloading/operator overloading/Program.cs
@@ -0,0 +1,70 @@
+﻿using System.Runtime.Intrinsics.Arm;
+
+namespace operator_overloading
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Complex c1 = new Complex() { Real = 1 , Image = 5};
+            Complex c2 = new Complex() { Real = 3 , Image = 1};
+            #region Operator Overloading
+            Console.WriteLine("==============(Operator Overloading)=================");
+            Console.WriteLine($"C1 = {c1.ToString()}");
+            Console.WriteLine($"C2 = {c2.ToString()} ");
+            Console.WriteLine("===============================");
+            Complex c3 = default;
+            c3 = c1 + c2;
+            Console.WriteLine($"C3 = C1 + C2 =  {c3.ToString()} ");
+            Console.WriteLine("===============================");
+            Complex c4 = default;
+            c4 = c1 - c2;           
+            Console.WriteLine($"C4 = C1 - C2 =  {c4.ToString()}"); 
+            Console.WriteLine("===============================");
+            Complex c5 = default;
+            c5 = c1 + 3;
+            Console.WriteLine($"C5 = C1 + 3 =  {c5.ToString()}");
+            Console.WriteLine("===============================");
+            Complex c6 = default;
+            c6 = 3 + c1 ;
+            Console.WriteLine($"C6 = 3 + C1 =  {c6.ToString()}");
+            Console.WriteLine("===============================");
+            Complex c7 = default;
+            c7 = c1 - 5;
+            Console.WriteLine($"C7 = C1 - 5 =  {c7.ToString()}");
+            Console.WriteLine("===============================");
+            Complex c8 = default;
+            c8 = 3 - c1;
+            Console.WriteLine($"C8 = 3 - C1 =  {c8.ToString()}");
+            #endregion
+
+            #region 
+            Console.WriteLine("==============Operator(++)=================");
+            c2++;
+            Console.WriteLine($"C2++ = {c2.ToString()}");
+            #endregion
+
+            #region 
+            Console.WriteLine("==============Operator(--)=================");
+            c2--;
+            Console.WriteLine($"C2-- = {c2.ToString()}");
+            #endregion
+            #region
+            Console.WriteLine("==============Operator(==)=================");
+            if (c1 == c2)
+                Console.WriteLine("c1 Is Equel c2");
+            else
+                Console.WriteLine("c1 Not Equel c2");
+            Console.WriteLine("===============================");
+            Complex complex1 = new Complex() { Real = 1 , Image = 1};
+            Complex complex2 = new Complex() { Real = 1, Image = 1 };
+            if (complex1 == complex2)
+                Console.WriteLine("Is Equel");
+            else
+                Console.WriteLine("Not Equel");
+
+            #endregion
+        }
+    }
+}
