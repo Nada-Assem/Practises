cfd21677d827d1fab848d5682cf6856d9070949f
Nada Assem <105822551+Nada-Assem@users.noreply.github.com>
2024-02-01 18:56:08 +0200

    Add files via upload

---
+++ ADO(.Net)/Day4.DAL/CourseDAL.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.Data.SqlClient;
+using System.Data;
+
+namespace Day4.DAL
+{
+
+    public class CourseDAL
+    {
+        SqlConnection sqlConnection = new SqlConnection("Data Source=Nada_Assem;Initial Catalog=ITI;Integrated Security=True;Trust Server Certificate=True");
+        SqlCommand cmd = new SqlCommand();
+
+        public CourseDAL()
+        {
+            sqlConnection.Open();
+            cmd.Connection = sqlConnection;
+        }
+        public DataTable GetAllCourse()
+        {
+            cmd.CommandText = "Select * From Course";
+            SqlDataAdapter adapter = new SqlDataAdapter(cmd);
+            DataTable dataTable = new DataTable();
+            adapter.Fill(dataTable);
+            return dataTable;
+        }
+        public int InsertIntoCourse(int Crs_Id , string Crs_Name ,int Crs_Duration , int Topic_Id )
+        {
+            cmd.CommandText = "insert into Course (Crs_Id , Crs_Name , Crs_Duration , Top_Id)" +
+                $"Values ( {Crs_Id}, '{Crs_Name}' ,{Crs_Duration} ,{Topic_Id} )";
+            int Rows = cmd.ExecuteNonQuery();
+            return Rows;
+        }
+        public int UpdateCourse(int Crs_Id, string Crs_Name, int Crs_Duration, int Topic_Id)
+        {
+            cmd.CommandText = "update Course set " +
+                $"Crs_Name = '{Crs_Name}' , Crs_Duration = {Crs_Duration} , Top_Id = {Topic_Id} where Crs_Id = {Crs_Id} ";
+            int Rows = cmd.ExecuteNonQuery();
+            return Rows;
+        }
+        public void DeleteCourse(int Crs_Id)
+        {
+            cmd.CommandText = $"Delete From Course Where Crs_Id = {Crs_Id}";
+            int Rows = cmd.ExecuteNonQuery();
+        }
+
+        public DataTable GetCourseByID(int Crs_Id)
+        {
+            cmd.CommandText = $"Select * From Course Where Crs_Id = {Crs_Id}";
+            SqlDataAdapter adapter = new SqlDataAdapter(cmd);
+            DataTable dataTable = new DataTable();
+            adapter.Fill(dataTable);
+            return dataTable;
+        }
+    }
+}
---
+++ ADO(.Net)/Day4.DAL/TopicDAL.cs
@@ -0,0 +1,65 @@
+﻿using Microsoft.Data.SqlClient;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlTypes;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day4.DAL
+{
+    public class TopicDAL
+    {
+        SqlConnection sqlConnection = new SqlConnection("Data Source=Nada_Assem;Initial Catalog=ITI;Integrated Security=True;Trust Server Certificate=True");
+        SqlCommand cmd = new SqlCommand();
+
+        public TopicDAL()
+        {
+            sqlConnection.Open();
+            cmd.Connection = sqlConnection;
+        }
+
+        public DataTable GetAllTopic()
+        {
+            cmd.CommandText = "Select * From Topic";
+            SqlDataAdapter adapter = new SqlDataAdapter(cmd);
+            DataTable dataTable = new DataTable();
+            adapter.Fill(dataTable);
+            return dataTable;
+        }
+        public DataTable GetTopicByID(int Top_Id)
+        {
+            cmd.CommandText = $"Select * From Topic Where Top_Id ={Top_Id}";
+            SqlDataAdapter adapter = new SqlDataAdapter(cmd);
+            DataTable dataTable = new DataTable();
+            adapter.Fill(dataTable);
+            return dataTable;
+        }
+        public int AddTopic(int Top_Id , string Top_Name )
+        {
+            cmd.CommandText = "insert into Topic( Top_Id ,Top_Name )" +
+                $"values ({Top_Id} , '{Top_Name}') ";
+            int Rows = cmd.ExecuteNonQuery();
+            return Rows;
+
+        }
+
+        public int DeleteTopic(int Top_Id)
+        {
+            cmd.CommandText = $"Delete from Topic where Top_Id ={Top_Id}";
+            int Rows = cmd.ExecuteNonQuery() ;
+            return Rows;
+        }
+
+        public int UpdateTopicByID(int Top_Id, string Top_Name) 
+        {
+            cmd.CommandText = "Update Topic " +
+                $"SET Top_Name = '{Top_Name}'" +
+                $"Where Top_Id = {Top_Id} ";
+            int Rows = cmd.ExecuteNonQuery();
+            return Rows;
+        }
+
+    }
+}
